4dbe5136607874df8f42b03c1a90b75f9b066c6b
ajibadeabd <ajibadeabd@gmail.com>
2020-12-16 01:56:38 +0100

    done with the nav bar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import header from "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header_logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+
+      <div className="header_search">
+        <input type="text" className="header_searchInput" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span class="header_optionLineOne">hello guest</span>
+          <span class="header_optionLineTwo">sign in </span>
+        </div>
+        <div className="header_option">
+          <span class="header_optionLineOne">return </span>
+          <span class="header_optionLineTwo">& orders </span>
+        </div>
+        <div className="header_option">
+          <span class="header_optionLineOne">your</span>
+          <span class="header_optionLineTwo"> prime </span>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/firsbase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD-TDsbr_Eyeu_EvEL4-MszJaRRd0IJS7c",
+  authDomain: "clone-c6663.firebaseapp.com",
+  projectId: "clone-c6663",
+  storageBucket: "clone-c6663.appspot.com",
+  messagingSenderId: "47785348972",
+  appId: "1:47785348972:web:13a5b1c7bc15890a2fed49",
+  measurementId: "G-TJQ7FJ1BGL"
+};
